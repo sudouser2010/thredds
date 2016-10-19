54a45442a8f3a5e2b5250f0e9ffa8938cb748191
Dennis Heimbigner <dmh@ucar.edu>
2016-10-19 11:28:00 -0600

    ck

--- httpservices/src/main/java/ucar/httpservices/HTTPUtil.java
+++ httpservices/src/main/java/ucar/httpservices/HTTPUtil.java
@@ -516,10 +516,12 @@ abstract public class HTTPUtil
         if(path == null) return null;
         StringBuilder b = new StringBuilder(path);
         canonicalpath(b);
-        if(b.charAt(0) == '/')
-            b.deleteCharAt(0);
-        if(hasDriveLetter(b))
-            b.delete(0,2);
+        if(b.length() > 0) {
+            if(b.charAt(0) == '/')
+                b.deleteCharAt(0);
+            if(hasDriveLetter(b))
+                b.delete(0, 2);
+        }
         return b.toString();
     }
 
